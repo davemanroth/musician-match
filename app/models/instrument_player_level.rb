fb7e70229b9d386823c96f24874e83997ecd6dc9
Dave Rothfarb <dave.rothfarb.webdev@gmail.com>
2019-08-25 18:35:26 -0400

    Instrument player level model associations and passing spec

--- app/models/instrument_player_level.rb
+++ app/models/instrument_player_level.rb
@@ -1,2 +1,5 @@
 class InstrumentPlayerLevel < ApplicationRecord
+  belongs_to(:user)
+  belongs_to(:instrument)
+  belongs_to(:player_level)
 end
--- spec/models/instrument_player_level_spec.rb
+++ spec/models/instrument_player_level_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe InstrumentPlayerLevel, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:user) }
+  it { should belong_to(:instrument) }
+  it { should belong_to(:player_level) }
 end
