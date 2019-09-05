b3a0c19ebfaf94bce815f62286f7c556178f9b2a
David Rothfarb <david@dr809.dfci.harvard.edu>
2019-09-05 17:38:45 -0400

    Added example to song spec that associates difficulty level with song, instrument. Everything passing

--- spec/models/song_instrument_spec.rb
+++ spec/models/song_instrument_spec.rb
@@ -2,15 +2,4 @@ require 'rails_helper'
 
 RSpec.describe SongInstrument, type: :model do
   it { should have_many(:song_instrument_levels) }
-
-  song = FactoryBot.create(:song_guitar)
-
-  it "should have a valid song_instrument association" do
-    expect(song.song_instruments).to_not be_empty
-  end
-
-  it "should have a level of 'hard' associated with it" do
-    si.song_levels << FactoryBot.build(:hard)
-    expect(si.song_levels[0].name).to eq("Hard")
-  end
 end
--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -6,35 +6,47 @@ RSpec.describe Song, type: :model do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:band) }
 
-  context "Valid band input" do
-    song = FactoryBot.create(:song_with_band)
-
+  context "Valid input" do
+    before(:each) do
+      @song = FactoryBot.create(:song)
+    end
     it "Should be a vaild song" do
-      expect(song).to be_valid
+      expect(@song).to be_valid
     end
 
     it "should have a valid name" do
-      expect(song.name).to_not be_blank
+      expect(@song.name).to_not be_blank
     end
 
     it "should have a band associated with it" do
-      expect(song.band.name).to_not be_blank
+      expect(@song.band.name).to_not be_blank
+    end
+
+    it "should have guitar as an instrument" do
+      guitar = FactoryBot.build(:instrument, :guitar)
+      @song.instruments << guitar
+      expect(@song.instruments[0].name).to eq("Guitar")
+    end
+
+    it "should have a difficulty level of 'Hard' associated with song and instrument" do
+      guitar = FactoryBot.build(:instrument, :guitar)
+      @song.instruments << guitar
+      hard = FactoryBot.build(:song_level, :hard)
+      sg = @song.song_instruments.find_by(instrument_id: guitar.id)
+      sg.song_levels << hard
+      expect(sg.song_levels.first.level).to eq("Hard")
     end
   end
 
-  context "Valid instrument input" do
-    song_guitar = FactoryBot.create(:song_guitar)
-byebug
-    it "should have guitar as an instrument" do
-      expect(song_guitar.instruments[0].name).to eq("Guitar")
-    end
-  end
 
   context "Invalid input" do
-    song = Song.create(name: "song")
+    before(:each) do
+      @song = FactoryBot.create(:song)
+    end
     
     it "Should be an invalid song" do
-      expect(song).to_not be_valid
+      @song.name = nil
+      expect(@song).to_not be_valid
     end
   end
 end
