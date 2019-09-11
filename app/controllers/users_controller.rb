0eb49463ce67278e8f69cb99525294b57d3a4598
David Rothfarb <david@dr809.dfci.harvard.edu>
2019-09-11 17:11:07 -0400

    Added tests in user controller spec for all valid/invalid post requests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,14 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     render json: @user, status: 200
   end
+
+  def create
+    @user = User.create!(user_params)
+    render json: @user, status: 201
+  end
+
+  private 
+    def user_params
+      params.permit(:first_name, :last_name, :username, :email, :password_digest)
+    end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,40 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe "Users API", type: :request do
+
+# Get all users
   describe "GET /users" do
     let!(:users) { FactoryBot.create_list(:user, 10) }
     before { get users_path }
 
-    it "should return a 200 success status code" do
+    it "returns a status code of 200" do
       expect(response).to have_http_status(200)
     end
 
-    it "should return 10 user entries" do
+    it "returns 10 user entries" do
       expect(json.length).to eq 10
     end
   end
 
+# Get a single user
   describe "GET /user/:id" do
+
     let(:user) { FactoryBot.create(:user) }
     before { get "/users/#{ user.id }" }
 
     context "when user record exists" do
-      it "should return status code 200" do
+      it "returns status code of 200" do
         expect(response).to have_http_status(200)
       end
 
-      it "should return the correct user" do
+      it "returns the correct user" do
         expect(json['id']).to eq user.id
       end
     end
       
     context "when user record does not exist" do
-      it "should return status code 404" do
+
+      it "returns status code 404" do
         get "/users/999"
         expect(response).to have_http_status(404)
       end
 
-      it "should return a not found message" do
+      it "returns a not found message" do
         get "/users/999"
         expect(response.body).to match(/Couldn't find User/)
       end
@@ -42,7 +47,39 @@ RSpec.describe "Users API", type: :request do
 
   end
 
-  describe "POST /users"
+# Create a user
+  describe "POST /users" do
+    let(:new_user) {{ first_name: 'Dave', last_name: 'Rothfarb', username: 'davemanroth', 
+                      email: 'asdfh', password_digest: 'asdf;on' }}
+
+    context "with valid input" do
+      before { post "/users", params: new_user }
+
+      it "returns a status code of 201" do
+        expect(response).to have_http_status(201)
+      end
+      
+      it "creates a new user" do
+        expect(json['last_name']).to eq('Rothfarb')
+      end
+    end
+
+    context "with invalid input" do
+      before { post "/users", params: { first_name: 'Dave' } }
+
+      it "returns a status code of 422" do
+        expect(response).to have_http_status(422)
+      end
+      
+      it "returns a validation failure message" do
+        expect(response.body).to include("Validation failed")
+      end
+    end
+
+  end
+
+# Update a user
   describe "PUT /users/:id"
+# Delete a user
   describe "DELETE /users/:id"
 end
