8f101b20d9b116683cd9213f101479bf8fe86f88
David Rothfarb <david@dr809.dfci.harvard.edu>
2019-09-11 14:39:31 -0400

    Figured out how to get request specs working. Added spec support file and exception_handler controller concern. Tested get users and get user/:id requests

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,10 +1,48 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
+RSpec.describe "Users API", type: :request do
   describe "GET /users" do
-    it "works! (now write some real specs)" do
-      get users_path
+    let!(:users) { FactoryBot.create_list(:user, 10) }
+    before { get users_path }
+
+    it "should return a 200 success status code" do
       expect(response).to have_http_status(200)
     end
+
+    it "should return 10 user entries" do
+      expect(json.length).to eq 10
+    end
   end
+
+  describe "GET /user/:id" do
+    let(:user) { FactoryBot.create(:user) }
+    before { get "/users/#{ user.id }" }
+
+    context "when user record exists" do
+      it "should return status code 200" do
+        expect(response).to have_http_status(200)
+      end
+
+      it "should return the correct user" do
+        expect(json['id']).to eq user.id
+      end
+    end
+      
+    context "when user record does not exist" do
+      it "should return status code 404" do
+        get "/users/999"
+        expect(response).to have_http_status(404)
+      end
+
+      it "should return a not found message" do
+        get "/users/999"
+        expect(response.body).to match(/Couldn't find User/)
+      end
+    end
+
+  end
+
+  describe "POST /users"
+  describe "PUT /users/:id"
+  describe "DELETE /users/:id"
 end
